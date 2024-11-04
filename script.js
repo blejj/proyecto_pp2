2fecf9f60b281e061fbb87e4800afe5707f64886
blejj <fbleileee@gmail.com>
2024-11-04 14:31:28 -0300

    Algunos arreglos

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let currentIndex = 0;
+const myCarouselElement = document.querySelector('#myCarousel')
 
-function showNextSlide() {
-  const slides = document.querySelector('.slides');
-  currentIndex = (currentIndex + 1) % slides.children.length;
-  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-// Cambia de imagen cada 3 segundos
-setInterval(showNextSlide, 3000);
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 10,
+  touch: false
+})
